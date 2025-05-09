ef3e37744e1b6b6e06baf50a8daa12b809460336
Grizzly-val <lanz.10donatos@gmail.com>
2025-05-09 11:22:40 +0800

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -7,8 +7,13 @@ class Sensor:
         self._current_reading = current_reading     #   - :  Protected Attribute. Can be directly accessed, but labeled as protected.
     
     @property
-    def get_unit(self):
+    def unit(self):
         return self.__unit
+    
+    @unit.setter
+    def set_new_unit(self, new_unit):
+        self.__unit = new_unit
+        
     
     @abstractmethod
     def display_status(self):
@@ -16,38 +21,37 @@ class Sensor:
 
 # Sublcass Donatos
 class Proximity_Sensor(Sensor):
-    def __init__(self, unit_number, distance_reading, prescence):
+    def __init__(self, unit_number, distance_reading, presence):
         super().__init__("Proximity Sensor", unit_number, distance_reading)
-        self.prescence = prescence
+        self._presence = presence
 
     @property
-    def get_distance(self):
+    def distance(self):
         return self._current_reading
     
     @property
-    def get_presence(self):
-        return self.prescence
+    def presence(self):
+        return self._presence
     
-    @property
     def get_m_distance(self):
-        return self.get_distance / 100
+        return self.distance / 100
 
     def display_status(self):
         print("=============================================")
-        print(f"{self.type}\nUnit Number: {self.get_unit}\nObject Detected: {self.get_presence}")
-        if self.prescence == True:
-            print(f"Distance: {self.get_distance}cm")
+        print(f"{self.type}\nUnit Number: {self.unit}\nObject Detected: {self.presence}")
+        if self.presence == True:
+            print(f"Distance: {self.distance}cm")
         else:
             print(f"Distance: N/A")
         print("=============================================")
 
     def is_too_close(self):
         minimum_distance = 20 # cm
-        if self.get_presence == True:
-            if self.get_distance <= minimum_distance:
+        if self.presence == True:
+            if self.distance <= minimum_distance:
                 print(f"Object too close!")
             else:
-                print(f"Object is {self.get_distance - minimum_distance}cm away from the minimum distance")
+                print(f"Object is {self.distance - minimum_distance}cm away from the minimum distance")
         else:
             print("No object within proximity")
 
@@ -59,3 +63,5 @@ class Proximity_Sensor(Sensor):
 proxy1 = Proximity_Sensor("ILYBBM", 34, True)
 proxy1.display_status()
 proxy1.is_too_close()
+proxy1.set_new_unit = "HAHAHA"
+proxy1.display_status()
